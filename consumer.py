233a68186fe676683e1c514552a7c15b3daf90b5
Ali Ahmed <a.ahmed@streaml.io>
2017-08-12 14:46:46 -0700

    Add comments to scripts

--- consumer.py
+++ consumer.py
@@ -1,21 +1,25 @@
-
 import pulsar
 import logging
 
+# Setup up basic logging
 logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', level=logging.DEBUG)
 
 logging.info('Connecting to Pulsar...')
 
+# Create a pulsar client instance with reference to the broker
 client = pulsar.Client('pulsar://localhost:6650')
+
+# Build a producer instance on a topic queue with an id
 consumer = client.subscribe('persistent://sample/standalone/ns1/wordcount', 'my-sub')
 
 logging.info('Created consumer')
 
 while True:
     try:
-        msg = consumer.receive(timeout_millis=5000)
+        msg = consumer.receive(timeout_millis=5000) # try and receive messages with a timeout
         logging.info("Received message '%s' id='%s'", msg.data(), msg.message_id())
-        consumer.acknowledge(msg)
+        consumer.acknowledge(msg) # send ack to pulsar for message delivery
     except Exception:
+    	# close client if no messages can be read within the timeout iteval
         logging.info("No message received, Closing connection")
-        client.close()
+        client.close() 
--- producer.py
+++ producer.py
@@ -3,24 +3,28 @@ import random
 from time import sleep
 import logging
 
-
+# This iterates continuously through a list sequence in random order
 def random_cycle(ls):
-    local_ls = ls[:]  # copy
+    local_ls = ls[:]  # create defensive copy
     while True:
         random.shuffle(local_ls)
         for e in local_ls:
             yield e
 
-
+# Setup up basic logging
 logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', level=logging.DEBUG)
 
 logging.info('Connecting to Pulsar...')
 
+# Create a pulsar client instance with reference to the broker
 client = pulsar.Client('pulsar://localhost:6650')
+
+# Build a producer instance on a specific topic
 producer = client.create_producer('persistent://sample/standalone/ns1/sentences')
 
 logging.info('Connected to Pulsar')
 
+# Collection of sentences to serve as random input sequence
 sentences = random_cycle([
             "the cow jumped over the moon",
             "an apple a day keeps the doctor away",
@@ -32,8 +36,8 @@ sentences = random_cycle([
 logging.info('Sending Messages ...')
 
 for sentence in sentences:
-    sleep(0.05)
+    sleep(0.05) # throttle messages with 50 ms delay
     logging.info('Sending message - %s ', sentence)
-    producer.send(sentence)
+    producer.send(sentence) # publish to pulsar
 
 client.close()
