2908b942ecffa7ced9503714406ce07208c7e25d
Luc Perkins <lucperkins@gmail.com>
2017-08-16 14:37:07 -0700

    use two search patterns

--- src/main/java/tutorial/FraudDetectionBolt.java
+++ src/main/java/tutorial/FraudDetectionBolt.java
@@ -7,14 +7,24 @@ import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FraudDetectionBolt extends BaseBasicBolt {
     @Override
     public void execute(Tuple tuple, BasicOutputCollector collector) {
         String ccNumber = tuple.getString(0);
 
-        if (ccNumber.startsWith("1")) {
-            collector.emit(new Values(ccNumber));
-        }
+        Set<String> patterns = new HashSet<String>() {{
+           add("123");
+           add("567");
+        }};
+
+        patterns.forEach(p -> {
+            if (ccNumber.contains(p)) {
+                collector.emit(new Values(ccNumber));
+            }
+        });
     }
 
     @Override
