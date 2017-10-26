a5636332d6cd7f5c3281fecc1610c3058264f9aa
Luc Perkins <lucperkins@gmail.com>
2017-10-25 21:16:43 -0700

    fix remaining gaps

--- consumer.py
+++ consumer.py
@@ -2,22 +2,19 @@ import pulsar
 import logging
 import sys
 
+TOPOLOGIES = ['word-count', 'fraud-detection']
 WORD_COUNT_TOPIC = 'persistent://sample/standalone/ns1/wordcount'
+FRAUD_DETECTION_TOPIC = 'persistent://sample/standalone/ns1/fraud'
 SUBSCRIPTION = 'my-sub'
 TIMEOUT = 10000
 
 # Setup up basic logging
 logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s', level=logging.DEBUG)
 
-def main(args):
-    logging.info('Connecting to Pulsar...')
 
-    # Create a pulsar client instance with reference to the broker
-    client = pulsar.Client('pulsar://localhost:6650')
-
-    consumer = client.subscribe(WORD_COUNT_TOPIC, SUBSCRIPTION)
-    logging.info('Created consumer for the topic %s', WORD_COUNT_TOPIC)
-
+def run_consumer(client, topic):
+    consumer = client.subscribe(topic, SUBSCRIPTION)
+    logging.info('Created consumer for the topic %s', topic)
     while True:
         try:
             # try and receive messages with a timeout of 10 seconds
@@ -26,6 +23,30 @@ def main(args):
             consumer.acknowledge(msg)  # send ack to pulsar for message consumption
         except Exception:
             logging.info("No message received in the last 10 seconds")
+
+
+def main(args):
+    if len(args) < 2:
+        topology = 'word-count'
+    else:
+        topology = args[1]
+
+    if not topology in TOPOLOGIES:
+        logging.fatal('The topology %s is not amongst the available topologies: %s', topology, ", ".join(TOPOLOGIES))
+        sys.exit(1)
+
+    logging.info('Connecting to Pulsar...')
+
+    # Create a pulsar client instance with reference to the broker
+    client = pulsar.Client('pulsar://localhost:6650')
+
+    if topology == 'word-count':
+        logging.info("Running the word count producer...")
+        run_consumer(client, WORD_COUNT_TOPIC)
+
+    elif topology == 'fraud-detection':
+        logging.info("Running the fraud detection producer...")
+        run_consumer(client, FRAUD_DETECTION_TOPIC)
 
     client.close()
 
